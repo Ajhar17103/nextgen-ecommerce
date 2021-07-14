a8f579dc6c1d976f784dc6b2108ed4f4e39d17c9
Muhammad Azharul Islam <contact.ajharislam@gmail.com>
2021-07-14 10:07:16 +0600

    update

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const cardfunc=()=>{
+    alert("Product add in carts");
+}
+
+const checkfunc=()=>{
+    alert("You have To  Singup ");
+     
+}
+const shipping=()=>{
+    alert('Order Successful,your product is going  for shipping')
+}
+
+const conform=()=>{
+    alert('Checkout successful')
+}
